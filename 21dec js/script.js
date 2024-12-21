487ceb3216ab8ea6c55ac42b3c8a1445319a540d
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2024-12-21 05:43:54 +0000

    todays class

---
+++ 21dec js/script.js
@@ -0,0 +1,79 @@
+// console.log("good morning");
+
+// var
+// let and const
+
+
+var a = 10
+
+// console.log(a);
+
+// typeof(typeof(anything)) --> string
+console.log(a,typeof(a), typeof(typeof(a)));
+// 10 'number' 'string'
+
+
+var b = String(a)
+console.log(b);
+console.log(typeof(b));
+// primitive data type ---> store only stack
+
+// number
+// string
+// bool
+// bigint
+// null
+// undefined
+// Symbol
+
+// non-primitive ----> store only heap(i.e ram)
+
+// array
+// object
+
+
+
+
+
+
+
+
+
+
+
+// string literals or template literals
+// ~ ->tilde
+// ``-> String
+// var name = "supriyo"
+// console.log(`Welcome to BCA ${name}`);
+
+
+
+
+
+
+// ishungry = false
+// console.log(typeof(ishungry));
+
+
+
+
+console.log(typeof(null));
+// object
+
+console.log(typeof(undefined));
+console.log(typeof('undefined'));
+// output :- undefined
+
+
+console.log(0/0); 
+// output : - NaN
+
+console.log(typeof(NaN));
+// output:- number
+
+
+console.log(isNaN(100)) //false
+console.log(isNaN("100d")) //true
+
+
