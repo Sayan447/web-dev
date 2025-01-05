a9bbbd072bbe69e883bdc96f7f1df11387f8af13
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2025-01-05 05:13:16 +0000

    u

---
+++ 04jan2025js/script.js
@@ -0,0 +1,116 @@
+// function is collection of statement and we can call the function
+// function defination
+
+
+// function callMe(){
+//     console.log()
+//     console.log()
+//     console.log()
+//     console.log()
+//     console.log()
+// }
+
+
+// callMe() // function calling
+
+
+
+
+// anonymus fuction
+// let callMePlease = function(){
+//     console.log("11")
+    
+// }
+
+
+// callMePlease()
+// callMePlease()
+// callMe()
+// output:- 11
+//  11
+//  callMe is not defined
+
+
+
+
+
+
+
+// formal parameter
+// let callMePlease = (a) =>{
+//     console.log("11")
+// }
+
+// function hello(a){
+//         console.log(a)
+// }
+
+
+
+// let a = 'hello'
+// hello(a)
+// callMePlease(a)
+// actual parameter 
+
+
+
+
+
+
+
+
+// function f1(){
+//     console.log('i am a f1() function');
+//     f1()
+    
+// }
+
+// function f2(f1){
+//     console.log('i am f2() function');
+//     f2()
+// }
+
+// f2()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 2 phase of hoisting
+// 1) creation phase
+// 2) Execution phase
+
+
+// in creation phase the entire code will be scaned and after that is does something called hoisting. the variables declared with let & const keyword will get assigned with a value called 'undefined'
+
+
+
+
+
