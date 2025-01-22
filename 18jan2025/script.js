d185876d1f78937cec14dc9751b7468c46652397
Sayan Chakraborty <69922781+Sayan447@users.noreply.github.com>
2025-01-22 14:51:23 +0000

    18jsn class

--- 18jan2025/script.js
+++ 18jan2025/script.js
@@ -64,7 +64,7 @@
 
 // function callme(func){
 //     let a = function(){
-//         return 'Hello'
+//         return 'Hello Hi'
 //     }
 //     func();
 //     return a;
@@ -76,5 +76,96 @@
 
 // console.log(callme(f1)());
 // console.log(a);
+// a is not defined
 
-// 53:48
+
+// let ,const block scope
+// var context scope
+
+
+
+
+
+
+// ARRAY
+// array is the index collection placed in contiguous memory location
+// we can store homoginious and hetrojinious
+
+// let arr = [10 , 20 , 36 , 59]
+// array bring some predefined APIs or function in js such as foreach,map,reduce,filter.
+
+// foreach is function in js
+
+// you can directly use forEach with an inline arrow function
+
+// arr.forEach((ele)=>{
+//     console.log(ele);
+// })
+
+// or
+
+// arr.forEach(ele)
+
+// function ele(i){
+//     console.log(i);   
+// }
+
+// 10 20 36 59
+
+// with arrow function
+
+// define the callback function
+// let k = (ele)=>{
+//     console.log(ele);   
+// }
+// pass the callback function to foreach
+// arr.forEach(k)
+
+
+
+// let arr = [45,87,9,365,68]
+
+// arr.forEach((ele,index,arr)=>console.log(ele , '---->' , index , '---->' , arr )
+// );
+
+// 45 ----> 0 ----> [ 45, 87, 9, 365, 68 ]
+// 87 ----> 1 ----> [ 45, 87, 9, 365, 68 ]
+// 9 ----> 2 ----> [ 45, 87, 9, 365, 68 ]
+// 365 ----> 3 ----> [ 45, 87, 9, 365, 68 ]
+// 68 ----> 4 ----> [ 45, 87, 9, 365, 68 ]
+
+
+
+
+// MAP
+
+
+// map and filter return new array
+// let arr = [45,87,9,365,68]
+// let g = arr.map((ele)=>{
+//     return ele + 5
+// })
+// console.log(g);
+// [ 50, 92, 14, 370, 73 ]
+// if we use foreach here
+// g.forEach((ele)=>{
+//     console.log(ele);  
+// })
+// 50
+// 92
+// 14
+// 370
+// 73
+
+
+
+
+
+// filter
+let arr = [45,87,9,365,68]
+
+let f = arr.filter((ele)=>{
+    return ele > 50
+})
+console.log(f);
+// [ 87, 365, 68 ]
